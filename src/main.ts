26357ca9d89a974fecd954d7ead3be3857b94e39
rishav <rishav.pokharel@studiomatrix.xyz>
2026-01-08 13:33:02 +0545

    cors commit added

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,12 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.enableCors({
+     origin: '*', // Allow all origins (for testing)
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
+    credentials: true, // if you use cookies or authorization headers
+  })
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
